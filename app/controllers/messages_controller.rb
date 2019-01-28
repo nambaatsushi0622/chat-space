7245b51fc5b238397e7ea84731a2be3059aa614d
難波 篤 <gokatu88@gmail.com>
2019-01-28 22:24:04 +0900

    ユーザー管理機能の追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,7 @@ class MessagesController < ApplicationController
   def index
    @Messages = "プログラミングなう"
   end
+  def create
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root 'messages#index'
   get 'messages' => 'messages#index'
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ end
 end
